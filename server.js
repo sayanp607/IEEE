e05a11d5b83713238ca7ce9ba8465eff36b797df
Sayan Paul <sayanp607@gmail.com>
2025-04-14 12:18:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -38,6 +38,8 @@ app.get("/count", (req, res) => {
 
 const authRoutes = require('./routes/authRoutes');
 app.use('/api/auth', authRoutes);
-
+app.get("/", (req, res) => {
+  res.send("🎉 Backend server is live and running!");
+});
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
